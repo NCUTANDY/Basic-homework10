a66adba0133e04af9af7de3619b303dd6d383a27
NCUTANDY <48341523+NCUTANDY@users.noreply.github.com>
2019-04-11 21:20:00 +0800

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<string.h>
+
+main()
+
+{
+
+  char C[5000];
+
+  char ouput;
+
+  printf("請輸入一個字串："); 
+
+  while(scanf("%s",C)==1) 
+
+      {
+
+         printf("處理過的字串為：",ouput);
+
+		 int a,N=strlen(C); 
+
+         for(a=0;a<N;a++)
+
+             if(C[a]>='a')  
+
+               printf("%c",C[a]-'a'+'A'); 
+
+             else 
+
+               printf("%c",C[a]-'A'+'a'); 
+
+      } 
+
+	system("pause");
+
+    return 0;  
+
+}
